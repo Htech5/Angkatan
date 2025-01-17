fe352f26ea3a33f038cd34192787598db400bd64
Codewarriorrs <isyamuhammad341@gmail.com>
2025-01-18 02:38:48 +0700

    add pic assets

---
+++ resources/views/components/PopBio1.blade.php
@@ -0,0 +1,66 @@
+<div id="modalContent" class="transform transition-all scale-95 opacity-0 w-[90vw] h-[90vh] shadow-lg flex flex-wrap bg-gradient-to-tl from-[#AD7D4F] from-60% to-[#EDB47E]  mx-auto">
+    <div class="w-[5vw] h-full bg-transparent mx-auto mt-20 relative">
+        <a href=""><img class="mx-auto mb-6 w-1/2" src="assets/uil_instagram.svg" alt=""></a>
+        <a href=""><img class="mx-auto mb-6 w-1/2" src="assets/mail.svg" alt=""></a>
+        <a href=""><img class="mx-auto mb-6 w-1/2" src="assets/telephone.svg" alt=""></a>
+        <a href=""><img class="mx-auto mb-4 w-1/3 absolute bottom-24 left-1/2 -translate-x-1/2" src="assets/burger.svg" alt=""></a>
+    </div>
+    <div class="w-1/3 bg-white h-full border"></div>
+    <div class="w-7/12  h-full relative">
+        <div id="closeModal" class="absolute right-5 top-3 hover:rotate-180 hover:duration-500 hover:scale-90 transition cursor-pointer text-white text-3xl">
+                &#128936;
+        </div>
+        <img src="assets/img.png"
+            class="h-3/4 bottom-0 lg:-left-1/4 md:-left-1/2  md:-translate-x-10 -left-2/3 absolute float-left"
+            alt="">
+        <div class="p-4">
+            <h4 class="lg:text-xl text-sm  mt-10 font-light text-orange-100">Muhammad Mudryck</h4>
+            <h1 class="lg:text-8xl sm:text-4xl font-bold text-white">MANG MUDRYCK</h1>
+        </div>
+        <div class="lg:text-xl text-sm  font-light text-orange-100 ml-10 mb-2">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, maxime. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Assumenda quaerat cupiditate quo consequatur? Repudiandae laborum est ea
+                eius. Numquam, cumque. </p>
+        </div>
+        <div class="lg:text-xl text-sm  font-light text-orange-100 ml-10 mb-2">
+            <ul class="flex flex-wrap">
+                <li class="w-1/3">Asal</li>
+                <li class="w-1/3">NIM</li>
+                <li class="w-1/3">TTL</li>
+            </ul>
+            <ul class="font-semibold flex flex-wrap">
+                <li class="w-1/3">London</li>
+                <li class="w-1/3">21120202002</li>
+                <li class="w-1/3">London, 30 February 2005</li>
+            </ul>
+        </div>
+        <!-- alamat kos -->
+        <div class="w-full m-4 text-orange-100 lg:text-xl text-sm ml-8 lg:ml-10">
+            <h1 class="font-light">Alamat Kos</h1>
+            <h1 class="font-semibold">Jl. Aja Terus Jadian Kagak No.999</h1>
+        </div>
+        <!-- alamat rumah -->
+        <div class="w-full m-4 text-orange-100 lg:text-xl text-sm  flex
+             justify-start ml-4 lg:ml-[90px]">
+            <div class=" ml-4 w-1/2">
+                <h1 class="font-light">Alamat Rumah</h1>
+                <h1 class="font-semibold">Jl. Terus Kapan Jadian</h1>
+            </div>
+            <div class="">
+                <h1 class="font-light">Ketinggian Rumah</h1>
+                <h1 class="font-semibold">10 MDPL</h1>
+            </div>
+        </div>
+        <!-- Hobi -->
+        <div class="w-full m-4 text-orange-100 lg:text-xl text-sm ">
+            <h1 class="  ml-4 lg:ml-[100px] font-light">Hobi</h1>
+            <h1 class=" ml-4 lg:ml-[100px] font-semibold">Nonton</h1>
+        </div>
+        <div class="w-full m-4 text-orange-100 lg:text-xl text-sm ">
+            <h1 class="ml-4 lg:ml-[100px] font-light">Tempat Makan Favorit</h1>
+            <h1 class="ml-4 lg:ml-[100px] font-semibold">SS</h1>
+        </div>
+
+    </div>
+
+</div>
